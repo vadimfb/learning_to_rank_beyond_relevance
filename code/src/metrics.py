8fcb4eb0d8e3317250c058402c3e018bc6defd39
Вадим Кислинский <vadimkislinskiy@MacBook-Pro-Vadim.local>
2021-04-03 14:23:46 +0300

    add metrics

---
+++ code/src/metrics.py
@@ -0,0 +1,169 @@
+from typing import List, Dict
+from math import log2
+from scipy.sparse.lil import lil_matrix
+
+def precision_at_k(relevant_lists: Dict[int, List[int]],
+                   recommend_lists: Dict[int, List[int]],
+                   k: int = 10) -> float:
+
+    hits = 0
+    n_recommendations = 0
+
+    for user, relevant_items in relevant_lists.items():
+
+        recommend_items = recommend_lists.get(user, [])
+        recommend_items = recommend_items[: k]
+        intersection_items = set(relevant_items) & set(recommend_items)
+        hits += len(intersection_items)
+        n_recommendations += k
+
+    precision = hits / n_recommendations
+
+    return precision
+
+
+def recall_at_k(relevant_lists: Dict[int, List[int]],
+                recommend_lists: Dict[int, List[int]],
+                k: int = 10) -> float:
+    hits = 0
+    n_relevants = 0
+
+    for user, relevant_items in relevant_lists.items():
+        recommend_items = recommend_lists.get(user, [])
+        recommend_items = recommend_items[: k]
+        intersection_items = set(relevant_items) & set(recommend_items)
+        hits += len(intersection_items)
+        n_relevants += len(relevant_items)
+
+    recall = hits / n_relevants
+
+    return recall
+
+def average_precision_at_k(relevant_items: List[int], recommend_items: List[int], k: int = 10) -> float:
+
+    relevant_items = set(relevant_items)
+
+    score = 0
+    for rank, item in enumerate(recommend_items[: k]):
+        if item in relevant_items:
+            intersection_items = relevant_items.intersection(recommend_items[: rank + 1])
+            hits = len(intersection_items)
+            p_at_rank = hits / (rank + 1)
+            score += p_at_rank
+
+    return score
+
+def mean_average_precision_at_k(relevant_lists: Dict[int, List[int]],
+                                recommend_lists: Dict[int, List[int]],
+                                k: int = 10) -> float:
+
+    map = 0
+    n_users = 0
+    for user, relevant_items in relevant_lists.items():
+        recommend_items = recommend_lists.get(user, [])
+        recommend_items = recommend_items[: k]
+        map += average_precision_at_k(relevant_items, recommend_items, k)
+        n_users += 1
+
+    map = map / n_users
+
+    return map
+
+def normalized_discounted_cumulative_gain_at_k(relevant_lists: Dict[int, List[int]],
+                                               recommend_lists: Dict[int, List[int]],
+                                               k: int = 10) -> float:
+
+    ndcg = 0
+    n_users = 0
+
+    for user, relevant_items in relevant_lists.items():
+        recommend_items = recommend_lists.get(user, [])
+        recommend_items = recommend_items[: k]
+        dcg = 0
+        idcg = 0
+        for rank, item in enumerate(recommend_items):
+            if item in relevant_items:
+                dcg += 1 / log2(rank + 1 + 1)
+            idcg += 1 / log2(rank + 1 + 1)
+
+        ndcg += dcg / idcg
+        n_users += 1
+
+    ndcg = ndcg / n_users
+
+    return ndcg
+
+def diversity_at_k(items: List[int],
+                   distancies: lil_matrix,
+                   k: int = 10) -> float:
+
+    diversity = 0
+
+    items = items[: k]
+    for item_i in items:
+        for item_j in items:
+            if item_i != item_j:
+                diversity += distancies[item_i, item_j]
+
+    diversity = diversity / (k * (k - 1))
+
+    return diversity
+
+def mean_diversity_at_k(item_lists: Dict[int, List[int]], distancies: lil_matrix, k: int = 10) -> float:
+
+    mean_diversity = 0
+    n_users = 0
+
+    for _, items in item_lists.items():
+        mean_diversity += diversity_at_k(items, distancies, k)
+        n_users += 1
+
+    mean_diversity = mean_diversity / n_users
+
+    return mean_diversity
+
+def novelty_at_k(items: List[int], item_long_tails: Dict[int, float], k: int = 10) -> float:
+
+    items = items[: k]
+
+    novelty = 0
+    for item in items:
+        novelty += item_long_tails[item]
+
+    novelty = novelty / k
+
+    return novelty
+
+def mean_novelty_at_k(item_lists: Dict[int, List[int]],
+                      item_long_tails: Dict[int, float],
+                      k: int = 10) -> float:
+
+    mean_novelty = 0
+    n_users = 0
+
+    for _, items in item_lists.items():
+        mean_novelty += novelty_at_k(items, item_long_tails, k)
+        n_users += 1
+
+    mean_novelty = mean_novelty / n_users
+
+    return mean_novelty
+
+def coverage_at_k(item_lists: Dict[int, List[int]], n_items: int, k: int = 10) -> float:
+
+    items_set = set()
+
+    for _, items in item_lists.items():
+        items_set.update(items[: k])
+
+    coverage = len(items_set) / n_items
+
+    return coverage
+
+def serendipity_at_k(items: List[int], **kwargs) -> float:
+    """TO DO"""
+    ...
+
+def mean_serendipity_at_k(items: List[int], **kwargs) -> float:
+    """TO DO"""
+    ...
--- code/src/utils.py
+++ code/src/utils.py
@@ -1,5 +1,27 @@
 import pandas as pd
+import numpy as np
 import os
+
+from sklearn.model_selection import KFold
+
+def reindex_user_and_item(df):
+    _reindex(df, 'user')
+    _reindex(df, 'item')
+
+def _reindex(df, col):
+    uniq_ids = df[col].unique()
+
+    def get_map(ids):
+        mapping = pd.DataFrame(
+            {
+                'old': ids,
+                'new': np.arange(ids.shape[0], dtype='uint32')
+            }
+        ).set_index('old')['new'].to_dict()
+
+        return mapping
+
+    df[col] = df[col].map(get_map(uniq_ids)).astype('uint32')
 
 def read_ml_1m(dir_path='../data/ml-1m'):
 
@@ -11,4 +33,31 @@ def read_ml_1m(dir_path='../data/ml-1m'):
     movies = pd.read_csv(movies_path, sep='::', header=None,
                          names=['item', 'title', 'genre'], engine='python')
 
-    return {'ratings': ratings, 'item_data': movies}
+    reindex_user_and_item(ratings)
+    _reindex(movies, 'item')
+
+    shape = (ratings['user'].max() + 1, ratings['item'].max() + 1)
+
+    return ratings, movies, shape
+
+def split_recsys_data(df, test_size=0.1):
+
+    df = df.sort_values('timestamp').reset_index(drop=True)
+    split_idx = int(np.ceil(df.shape[0] * (1 - test_size)))
+    train_set, test_set = df.iloc[:split_idx, :].copy(), df.iloc[split_idx:, :].copy()
+
+    train_users = set(train_set['user'].unique())
+    test_set = test_set.query('user in @train_users').reset_index(drop=True)
+
+    return train_set, test_set
+
+def split_by_user_folds(df, n_folds=3, seed=0, shuffle=True):
+
+    uniq_users = df['user'].unique()
+    kf = KFold(n_splits=n_folds, random_state=seed, shuffle=shuffle)
+    folds = [
+        df[df['user'].isin(uniq_users[test_ind])].reset_index(drop=True)
+        for _, test_ind in kf.split(uniq_users)
+    ]
+
+    return folds
